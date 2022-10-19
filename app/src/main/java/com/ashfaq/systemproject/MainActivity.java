9c1c6d4ca9e004e43dd46ac6bad61c2cf6cbcb8e
Ashfaq Mahmud <ash.mse21@gmail.com>
2022-10-19 06:06:15 +0600

    initial commit

---
+++ app/src/main/java/com/ashfaq/systemproject/MainActivity.java
@@ -0,0 +1,57 @@
+package com.ashfaq.systemproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    EmployerFragment employerFragment = new EmployerFragment();
+    DashboardFragment dashboardFragment = new DashboardFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView  = findViewById(R.id.bottomNavigationView);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+
+        BadgeDrawable badgeDrawable = bottomNavigationView.getOrCreateBadge(R.id.dashboard);
+        badgeDrawable.setVisible(true);
+        badgeDrawable.setNumber(8);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                        return true;
+                    case R.id.employer:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,employerFragment).commit();
+                        return true;
+                    case R.id.dashboard:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,dashboardFragment).commit();
+                        return true;
+                    case R.id.profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,profileFragment).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
+    }
+}
