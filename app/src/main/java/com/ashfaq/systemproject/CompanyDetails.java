084330d5eb2ee20954df44ab29932cc077695306
Ashfaq Mahmud <ash.mse21@gmail.com>
2022-12-11 05:18:56 +0600

    line chart working

--- app/src/main/java/com/ashfaq/systemproject/CompanyDetails.java
+++ app/src/main/java/com/ashfaq/systemproject/CompanyDetails.java
@@ -8,12 +8,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+
 import android.widget.TextView;
 
-import com.google.android.material.imageview.ShapeableImageView;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -28,12 +31,11 @@ public class CompanyDetails extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_company_details, container, false);
 
-        ImageView back = view.findViewById(R.id.back);
         FirebaseDatabase node =FirebaseDatabase.getInstance();
         DatabaseReference reference= node.getReference();
         TextView tname,tmail,ttype,tabout,tloc,tsize;
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        FirebaseUser user = mAuth.getCurrentUser();
+        //FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        //FirebaseUser user = mAuth.getCurrentUser();
 
         tname = view.findViewById(R.id.username);
         tmail = view.findViewById(R.id.usermail);
@@ -53,6 +55,12 @@ public class CompanyDetails extends Fragment {
                 String loc = snapshot.child(uid).child("Location").getValue(String.class);
                 String about = snapshot.child(uid).child("about").getValue(String.class);
 
+                int val18 = snapshot.child(uid).child("Economical value").child("2018").getValue(Integer.class);
+                int val19 = snapshot.child(uid).child("Economical value").child("2019").getValue(Integer.class);
+                int val20 = snapshot.child(uid).child("Economical value").child("2020").getValue(Integer.class);
+                int val21 = snapshot.child(uid).child("Economical value").child("2021").getValue(Integer.class);
+                int val22 = snapshot.child(uid).child("Economical value").child("2022").getValue(Integer.class);
+
                 tname.setText(name);
                 tmail.setText(mail);
                 ttype.setText(cat);
@@ -60,6 +68,15 @@ public class CompanyDetails extends Fragment {
                 tloc.setText(loc);
                 tsize.setText(size);
 
+                GraphView graph = (GraphView) view.findViewById(R.id.graph);
+                LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                        new DataPoint(2018, val18),
+                        new DataPoint(2019, val19),
+                        new DataPoint(2020, val20),
+                        new DataPoint(2021, val21),
+                        new DataPoint(2022, val22)
+                });
+                graph.addSeries(series);
             }
 
             @Override
@@ -68,12 +85,6 @@ public class CompanyDetails extends Fragment {
             }
         });
 
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //
-            }
-        });
 
 
         return view;
